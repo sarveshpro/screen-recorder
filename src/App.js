b9c51f353254bc636d25734d131ecce738a53519
Sarvesh Warge <sarveshwarge@gmail.com>
2020-08-20 11:53:11 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import useScreenRecording from "use-screen-recording";
+import "./App.css";
 
 function App() {
+  const { isRecording, recording, toggleRecording } = useScreenRecording();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Screen Recorder</h1>
+      <button className="btn-primary" onClick={toggleRecording}>
+        {isRecording ? "Stop" : "Start Recording"}
+      </button>
+
+      {!!recording && (
+        <div className="replay-container">
+          <video
+            className="video-preview"
+            autoPlay
+            src={recording && URL.createObjectURL(recording)}
+          />
+
+          <div className="share-recording">
+            <a
+              type="button"
+              className="btn btn-social"
+              download="Screen Recording"
+              href={recording && URL.createObjectURL(recording)}
+            >
+              Share
+            </a>
+            <a
+              type="button"
+              className="btn btn-secondary"
+              download="Screen Recording"
+              href={recording && URL.createObjectURL(recording)}
+            >
+              Download
+            </a>
+            <a
+              type="button"
+              className="btn btn-youtube"
+              download="Screen Recording"
+              href={recording && URL.createObjectURL(recording)}
+            >
+              Upload
+            </a>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
